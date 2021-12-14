e7778ff036f83d59e3531f02679987280fa34146
David Abarca <david_abarca@icloud.com>
2021-12-14 22:52:07 +0100

    Translate input into nato alphabet

---
+++ main.py
@@ -0,0 +1,45 @@
+user_input = input("Enter your word to be translated: ")
+
+nato_alphabet = {
+    "a": "alpha",
+    "b": "bravo",
+    "c": "charlie",
+    "d": "delta",
+    "e": "echo",
+    "f": "foxtrot",
+    "g": "golf",
+    "h": "hotel",
+    "i": "india",
+    "j": "juliett",
+    "k": "kilo",
+    "l": "lima",
+    "m": "mike",
+    "n": "november",
+    "o": "oscar",
+    "p": "papa",
+    "q": "qubec",
+    "r": "romea",
+    "s": "sierra",
+    "t": "tango",
+    "u": "uniform",
+    "v": "victor",
+    "w": "whiskey",
+    "x": "xray",
+    "y": "yankee",
+    "z": "zulu",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
+    "0": "zero"
+}
+
+for char in user_input:
+    if char in nato_alphabet:
+        print(nato_alphabet.get(char))
+
